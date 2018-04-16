5338ef48884f5a0d7d1a54f1138864b67ad30058
xiaofeier312 <xiaofeier312@xiaofeier312deMacBook-Pro.local>
2018-04-16 20:26:55 +0800

    add basic code

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRECT_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProductionConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1/tushare?charset=utf8'
+
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://root:root@127.0.0.1/tushare?charset=utf8'
+
+config = {
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': TestingConfig
+}
