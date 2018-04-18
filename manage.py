6247ef21e6dfea35f884e1f2c43f668ea9cc904c
xiaofeier312 <xiaofeier312@xiaofeier312deMacBook-Pro.local>
2018-04-18 21:16:55 +0800

    add command in manager

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, mod = APIModules, pro = APIProjects)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 migrate = Migrate(app,db)
@@ -23,6 +23,26 @@ def init_db():
 @manager.command
 def create_db():
     db.create_all(bind=None)
+
+@manager.command
+def init_data():
+    p1 = APIProjects()
+    p1.name = '个人中心'
+    p2 = APIProjects()
+    p2.name = '企业家'
+    m1 = APIModules()
+    m1.name = '个人中心-mobile war'
+    m1.projectID = 1
+    m1.remark = 'module of mobile war'
+    m2 = APIModules()
+    m2.name = 'portal war'
+    m2.projectID = 2
+    m2.remark = 'remark of protal war'
+    db.session.add(p1)
+    db.session.add(p2)
+    db.session.add(m1)
+    db.session.add(m2)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
