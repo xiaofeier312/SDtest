de34149ce6890d006ce8378c3177e7e29070f06b
xiaofeier312 <xiaofeier312@xiaofeier312deMacBook-Pro.local>
2018-04-22 22:33:59 +0800

    Add data class to flask_admin page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_admin import Admin
+from app.admin.views import CustomModelView
 
 
 db = SQLAlchemy()
-
+app_admin = Admin(name='API auto')
 
 def create_app(config_name):
     """Use factory to product app"""
@@ -12,6 +14,18 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     db.init_app(app)
+
+    #flask_admin
+    app_admin.init_app(app)
+    from app.models import APIProjects, APIModules, APICases, APIDoc, TomcatEnv, CasesVerify
+    app_admin.add_view(CustomModelView(APIProjects,db.session,category='新增'))
+    app_admin.add_view(CustomModelView(TomcatEnv, db.session, category='新增'))
+    app_admin.add_view(CustomModelView(APIModules, db.session, category='新增'))
+    app_admin.add_view(CustomModelView(APIDoc, db.session, category='新增'))
+    app_admin.add_view(CustomModelView(APICases, db.session, category='新增'))
+    app_admin.add_view(CustomModelView(CasesVerify, db.session, category='新增'))
+
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
---
+++ app/admin/views.py
@@ -0,0 +1,7 @@
+from flask_admin.contrib.sqla import ModelView
+
+
+
+class CustomModelView(ModelView):
+    """View function of Flask-Admin for Models page."""
+    pass
