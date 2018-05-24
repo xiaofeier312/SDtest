79a391762c8c59c1566f31658682261127c0850d
xiaofeier312 <xiaofeier312@xiaofeier312deMacBook-Pro.local>
2018-05-24 19:41:44 +0800

    add func get_module_by_id

--- app/main/services.py
+++ app/main/services.py
@@ -10,9 +10,9 @@ class SDData(object):
         print(all_project)
         return all_project
 
-    def get_all_models(self):
-        all_models = APIModules.query.all()
-        return all_models
+    def get_all_modules(self):
+        all_modules = APIModules.query.all()
+        return all_modules
 
     def get_all_doc(self):
         all_doc = APIDoc.query.all()
@@ -22,5 +22,11 @@ class SDData(object):
         all_cases = APICases.query.all()
         return all_cases
 
-    def get_module_by_id(self):
-        
+    def get_module_by_id(self,project_id,project_name):
+        """past project_name, and get it's modules"""
+        if id:
+            module = APIModules.query.fileter(id=project_id)
+        else:
+            module = []
+        return module
+
