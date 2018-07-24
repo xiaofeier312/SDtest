52595c8623d50147d662122f2b1859d03a783905
xiaofeier <xiaofeier312@126.com>
2018-07-24 21:21:59 +0800

    add config

--- config.py
+++ config.py
@@ -14,8 +14,13 @@ class Config:
 
 class ProductionConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1/tushare?charset=utf8'
-
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alimysql:alimysql7933@47.98.133.163/sdauto?charset=utf8'
+    SQLALCHEMY_BINDS = {
+        # 'mock_data': 'mysql+pymysql://root:adadad1@127.0.0.1:3306/mock?charset=utf8',
+        # 'mysql_117.226': 'mysql+pymysql://ent_all:ent@172.16.117.226:3306/ent_portal?charset=utf8'
+    }
+    SQLALCHEMY_ECHO = True # set sql echo = true
 
 
 class TestingConfig(Config):
